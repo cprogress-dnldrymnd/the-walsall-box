bdaafee94a354826228e470055c5db3086854fb1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-30 00:48:00 +0800

    Update functions.php

--- wallbox-child/functions.php
+++ wallbox-child/functions.php
@@ -198,8 +198,8 @@ function action_wp_footer()
 {
 	?>
 	<script>
-		var theArray = [1, 5, 10, 12];
-		var goal = 12;
+		var theArray = [1, 5, 9, 13];
+		var goal = 11;
 		var closest = null;
 
 		jQuery.each(theArray, function() {
