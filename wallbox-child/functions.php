b405bfc91d581dd2272a3fd3fefcff4e688568b8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-30 00:01:27 +0800

    Update functions.php

--- wallbox-child/functions.php
+++ wallbox-child/functions.php
@@ -129,7 +129,7 @@ function action_woocommerce_single_variation()
 						<?php foreach ($pricing_rules as $pricing_rule) { ?>
 							<?php if ($pricing_rule['variation_rules']['args']['variations'][0] == $product_variation->get_id()) { ?>
 								<?= $pricing_rule['rules'][0]['from'] ?>
-								<?= var_dump($pricing_rule['rules'][0]) ?>
+								<?= var_dump($pricing_rule['rules']) ?>
 								lll
 							<?php } ?>
 						<?php } ?>
