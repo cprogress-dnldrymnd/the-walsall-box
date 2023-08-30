f00ee7f3c0acaad7730db051e04b44dd2790e1e6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-30 12:38:44 +0800

    cc

--- wallbox-child/functions.php
+++ wallbox-child/functions.php
@@ -286,6 +286,7 @@ function subpages()
 					<div class="heading-box">
 						<h4><?php echo $childPage->post_title; ?></h4>
 					</div>
+					<span class="qodef-shortcode qodef-m  qodef-button qodef-layout--textual  qodef-html--link" href="https://thewalsallbox.theprogressteam.co.uk/2015/10/09/new-address/" target="_self"> <span class="qodef-m-text">Read More</span></span>
 				</a>
 			</div>
 
