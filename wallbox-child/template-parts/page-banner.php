4462216c839878fa3d93dd41b7e6e3cb3040ccd5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-30 13:04:03 +0800

    3232

--- wallbox-child/template-parts/page-banner.php
+++ wallbox-child/template-parts/page-banner.php
@@ -1,7 +1,12 @@
-<section class="page-banner">
+<?php
+$image_url = get_the_post_thumbnail_url();
+?>
+
+
+<section class="page-banner" style="background-image: url(<?= $image_url ?>);">
     <div class="qodef-m-inner">
         <div class="qodef-m-content qodef-content-grid ">
-            sdsdsds
+
         </div>
     </div>
 </section>
