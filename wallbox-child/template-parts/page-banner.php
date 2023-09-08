31186130b16eea6fe6136cb187ef5b0f2389cb71
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-08 15:13:23 +0800

    Update page-banner.php

--- wallbox-child/template-parts/page-banner.php
+++ wallbox-child/template-parts/page-banner.php
@@ -1,12 +1,15 @@
 <?php
 $image_url = get_the_post_thumbnail_url();
-?>
+if (!is_front_page() && is_page() && !is_page_template()) {
+    ?>
 
 
-<section class="page-banner" style="background-image: url(<?= $image_url ?>);">
-    <div class="qodef-m-inner">
-        <div class="qodef-m-content qodef-content-grid ">
-            <h1> <?php the_title() ?></h1>
+    <section class="page-banner" style="background-image: url(<?= $image_url ?>);">
+        <div class="qodef-m-inner">
+            <div class="qodef-m-content qodef-content-grid ">
+                <h1> <?php the_title() ?></h1>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+
+<?php } ?>
