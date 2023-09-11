812c244f7e64d0e392413dc400199f1f9d4c5f44
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-11 19:21:38 +0800

    cc

--- wallbox-child/template-parts/page-banner.php
+++ wallbox-child/template-parts/page-banner.php
@@ -1,10 +1,10 @@
 <?php
 $image_url = get_the_post_thumbnail_url();
-if (!is_front_page() && is_page() && !is_page_template() ) {
-    ?>
+if (!is_front_page() && is_page() && !is_page_template()) {
+?>
 
 
-    <section class="page-banner" style="background-image: url(<?= $image_url ?>);">
+    <section class="page-banner <?= $image_url ? 'with-background' : 'no-background' ?>" <?= $image_url ? 'style="background-image: url(' . $image_url . ')"' : '' ?>>
         <div class="qodef-m-inner">
             <div class="qodef-m-content qodef-content-grid ">
                 <h1> <?php the_title() ?></h1>
