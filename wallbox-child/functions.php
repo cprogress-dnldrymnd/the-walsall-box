aafd8de91d8dfb3e48fbb573628a6132c1588eeb
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-14 21:47:42 +0800

    Update functions.php

--- wallbox-child/functions.php
+++ wallbox-child/functions.php
@@ -209,8 +209,9 @@ function action_wp_footer()
 	<script>
 		jQuery(document).ready(function() {
 			jQuery('a[href="#tab-enquire_tab"]').click(function(e) {
-				console.log('sdsdsd');
-				jQuery('a[href="#tab-enquire_tab"]').click();
+				jQuery('#tab-title-enquire_tab').addClass('active');
+				jQuery('.woocommerce-Tabs-panel').css('display', 'none');
+				jQuery('#tab-enquire_tab').css('display', 'block');
 				e.preventDefault();
 			});
 		});
