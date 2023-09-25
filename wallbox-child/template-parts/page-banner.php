c28dfeb4c41dcddba5cc0da0d6d316205336667d
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-25 23:43:44 +0800

    Update page-banner.php

--- wallbox-child/template-parts/page-banner.php
+++ wallbox-child/template-parts/page-banner.php
@@ -1,13 +1,19 @@
 <?php
 $image_url = get_the_post_thumbnail_url();
 if (!is_front_page() && is_page() && !is_page_template() || is_shop()) {
+
+    if(is_shop()) {
+        $title = 'SHOP';
+    } else {
+        $title = get_the_title();
+    }
 ?>
 
 
     <section class="page-banner <?= $image_url ? 'with-background' : 'no-background' ?>" <?= $image_url ? 'style="background-image: url(' . $image_url . ')"' : '' ?>>
         <div class="qodef-m-inner">
             <div class="qodef-m-content qodef-content-grid ">
-                <h1> <?php the_title() ?></h1>
+                <h1> <?= $title ?></h1>
             </div>
         </div>
     </section>
