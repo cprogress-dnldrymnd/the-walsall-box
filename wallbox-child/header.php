1d9b41900ddca582c63709f8866eac802f0dd1f6
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-01 04:05:13 +0800

    Update header.php

--- wallbox-child/header.php
+++ wallbox-child/header.php
@@ -8,17 +8,6 @@
 
 	<?php wp_head(); ?>
 
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-1052367982"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-		gtag('config', 'AW-1052367982');
-	</script>
 </head>
 
 
